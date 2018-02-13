5b5b6d0f2335a087069cf02538d37d4f8e1c803e
spy <sp831031@gmail.com>
2018-02-13 10:36:21 +0800

    S18L2as3

--- js/index.js
+++ js/index.js
@@ -78,14 +78,14 @@ class Hero extends BaseCharacter {
     _this.id = setInterval(function() {
       
       if (i == 1) {
-         
+         _this.element.getElementsByClassName("effect-image")[0].style.display = "block";
           _this.element.getElementsByClassName("heal-text")[0].classList.add("healed");
           _this.element.getElementsByClassName("heal-text")[0].textContent = heal;
         }        
-   
+        _this.element.getElementsByClassName("effect-image")[0].src = 'images/effect/heal/'+ i +'.png';
         i++;
       if (i > 8) {
-        
+          _this.element.getElementsByClassName("effect-image")[0].style.display = "none";
           _this.element.getElementsByClassName("heal-text")[0].classList.remove("healed");
           _this.element.getElementsByClassName("heal-text")[0].textContent = "";
           clearInterval(_this.id);
